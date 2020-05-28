a6865153686b356693a0d2416eaaf1f0a4408d6a
aidanakad <aydana0504@gmail.com>
2020-05-28 19:18:15 +0600

    returned development component

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -5,7 +5,7 @@ import Development from '../Development';
 export default function Resume() {
   return (
     <PageWrapper>
-      <div>Lorem ipsum dolor sit amet.</div>
+      <Development/>
     </PageWrapper>
   );
 }
