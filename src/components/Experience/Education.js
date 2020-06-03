b8876bea19d7a75393b143a22d8b4a8af8b4f919
aidanakad <aydana0504@gmail.com>
2020-06-03 15:19:50 +0600

    created education file

---
+++ src/components/Experience/Education.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from './experience.module.css'
+
+export default function Education() {
+    return (
+      <div></div>
+    );
+  }
--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import PageWrapper from '../PageWrapper'
 import Information from './Information'
+import Education from './Education'
 import {Container, Row, Col} from 'react-bootstrap'
-import mypic from '../../img/personal-img.JPG'
+import mypic from '../../img/personal-img-min.JPG'
 import style from './experience.module.css'
 
 export default function Resume() {
   return (
     <PageWrapper>
       <Container>
-      
-          
-            <h2 style={{ padding: "10px 20px", textAlign: "center", margin:"24px 0 24px 0"}}>ABOUT ME</h2>
-            
-            <Row className={style.box}>
-              <Col xs={12} md={6}><img src = {mypic} alt="avatar" className={style.img} /></Col>
-              <Col xs={12} md={6}><Information/></Col>
-            </Row>
-        
+        <h2 style={{ padding: "10px 20px", textAlign: "center", margin:"24px 0 24px 0"}}>ABOUT ME</h2>         
+        <Row className={style.box}>
+          <Col xs={12} md={6}><img src = {mypic} alt="avatar" className={style.img} /></Col>
+          <Col xs={12} md={6}><Information/></Col>
+          <Education/>
+        </Row>
       </Container>
       
     </PageWrapper>
