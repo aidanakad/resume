a2901d8bf5d8a616c74fa781740bc5e2abd6e548
aidanakad <aydana0504@gmail.com>
2020-06-17 18:16:26 +0600

    added contact from

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import PageWrapper from '../PageWrapper'
-import Development from '../Development';
+import Form from './Form'
+import Messanger from './Messanger'
 
 export default function Contact() {
   return (
     <PageWrapper>
-      <Development/>
+      <h2>GET IN TOUCH</h2>
+      <h6>If you would like to discuss projects, work, partnership or just want to say Hello, I am always open for conversation. You can contact me right from here.</h6>
+      <Form/>
+      <Messanger/>
     </PageWrapper>
   );
 }
