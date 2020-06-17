d96b9c103bfa957e8ef4d0953d17f1251e6bfd9f
aidanakad <aydana0504@gmail.com>
2020-06-17 22:19:55 +0600

    created portfolio components and added path for them

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import About from'./components/About'
 import Experience from'./components/Experience'
 import Portfolio from'./components/Portfolio'
-// import ContactList from './components/ContactList'
+import ContactList from './components/ContactList'
+import ToDoList from './components/ToDoList'
 import Contact from './components/Contact';
 import NotFound from './components/NotFound'
 
@@ -17,8 +18,8 @@ function App() {
           <Route path="/experience" component={Experience} exact />
           <Route path="/portfolio" component={Portfolio} exact/>
           <Route path="/contact" component={Contact} exact/>
-          {/* <Route path="/portfolio/contact-list" component={ContactList} exact/> */}
-          
+          <Route path="/portfolio/contact-list" component={ContactList} exact/>
+          <Route path="/portfolio/todo-list" component={ToDoList} exact/>
           <Route component={NotFound}/>
         </Switch>
       </BrowserRouter>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import PageWrapper from '../PageWrapper'
-import Development from '../Development';
-import {NavLink} from 'react-router-dom'
+// import {NavLink} from 'react-router-dom'
+import { Container } from 'react-bootstrap';
+import Card from './Card'
+import contact from '../../img/contact.png'
+import todo from '../../img/todo.png'
+import style from './portfolio.module.css'
+
 
 export default function Portfolio() {
   return (
     <PageWrapper>
-      <div>
-
-      <NavLink  to='/portfolio/contact-list'>Contact List</NavLink>
-      </div>
+      <Container>
+        <h2 className={style.h2} style={{textAlign:"center"}}>MY PORTFOLIO</h2>
+          <Card 
+            link="/portfolio/contact-list"
+            title= 'Contact List'
+            desc= "In this project I realized simple logic of contact list, by using react's useState."
+            image = {contact}
+          />
+          <Card 
+            link="/portfolio/todo-list"
+            title= 'To Do List'
+            desc= "In order to create this app, I used react's useReduser. The app lets add tasks and set priority."
+            image={todo}
+          />
+      </Container>
     </PageWrapper>
   );
 }
