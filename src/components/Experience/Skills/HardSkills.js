3dd6e87a533e17bc701c448df7bd55c116375d5b
aidanakad <aydana0504@gmail.com>
2020-06-18 17:21:18 +0600

    corrected skills info

--- src/components/Experience/Skills/HardSkills.js
+++ src/components/Experience/Skills/HardSkills.js
@@ -9,46 +9,36 @@ export default function HardSkils() {
           <div className={style.skill}>
               <div className={style.top}>
                   <div className={style.skillName}>HTML</div>
-                  <div className={style.percent}>95%</div>
-              </div>
-              <div className={style.wrapper} >
-                  <div className={style.percentage} style={{width:'95%', height: '18px', borderRadius:'6px'}}></div>
-              </div>
-          </div>
-
-          <div className={style.skill}>
-              <div className={style.top}>
-                  <div className={style.skillName}>CSS</div>
                   <div className={style.percent}>90%</div>
               </div>
-              <div className={style.wrapper}>
+              <div className={style.wrapper} >
                   <div className={style.percentage} style={{width:'90%', height: '18px', borderRadius:'6px'}}></div>
               </div>
           </div>
 
           <div className={style.skill}>
               <div className={style.top}>
-                  <div className={style.skillName}>JavaScript</div>
-                  <div className={style.percent}>70%</div>
+                  <div className={style.skillName}>CSS</div>
+                  <div className={style.percent}>87%</div>
               </div>
               <div className={style.wrapper}>
-                  <div className={style.percentage} style={{width:'70%', height: '18px', borderRadius:'6px'}}></div>
+                  <div className={style.percentage} style={{width:'87%', height: '18px', borderRadius:'6px'}}></div>
+              </div>
+          </div>
+
+          <div className={style.skill}>
+              <div className={style.top}>
+                  <div className={style.skillName}>JavaScript</div>
+                  <div className={style.percent}>65%</div>
+              </div>
+              <div className={style.wrapper}>
+                  <div className={style.percentage} style={{width:'65%', height: '18px', borderRadius:'6px'}}></div>
               </div>
           </div>
 
           <div className={style.skill}>
               <div className={style.top}>
                   <div className={style.skillName}>React</div>
-                  <div className={style.percent}>80%</div>
-              </div>
-              <div className={style.wrapper}>
-                  <div className={style.percentage} style={{width:'80%', height: '18px', borderRadius:'6px'}}></div>
-              </div>
-          </div>
-
-          <div className={style.skill}>
-              <div className={style.top}>
-                  <div className={style.skillName}>Redux</div>
                   <div className={style.percent}>60%</div>
               </div>
               <div className={style.wrapper}>
@@ -58,31 +48,41 @@ export default function HardSkils() {
 
           <div className={style.skill}>
               <div className={style.top}>
-                  <div className={style.skillName}>Bootstap</div>
-                  <div className={style.percent}>90%</div>
+                  <div className={style.skillName}>Redux</div>
+                  <div className={style.percent}>40%</div>
               </div>
               <div className={style.wrapper}>
-                  <div className={style.percentage} style={{width:'90%', height: '18px', borderRadius:'6px'}}></div>
+                  <div className={style.percentage} style={{width:'40%', height: '18px', borderRadius:'6px'}}></div>
+              </div>
+          </div>
+
+          <div className={style.skill}>
+              <div className={style.top}>
+                  <div className={style.skillName}>Bootstap</div>
+                  <div className={style.percent}>70%</div>
+              </div>
+              <div className={style.wrapper}>
+                  <div className={style.percentage} style={{width:'70%', height: '18px', borderRadius:'6px'}}></div>
               </div>
           </div>
 
           <div className={style.skill}>
               <div className={style.top}>
                   <div className={style.skillName}>CorelDraw</div>
-                  <div className={style.percent}>70%</div>
+                  <div className={style.percent}>60%</div>
               </div>
               <div className={style.wrapper}>
-                  <div className={style.percentage} style={{width:'70%', height: '18px', borderRadius:'6px'}}></div>
+                  <div className={style.percentage} style={{width:'60%', height: '18px', borderRadius:'6px'}}></div>
               </div>
           </div>
 
           <div className={style.skill}>
               <div className={style.top}>
                   <div className={style.skillName}>Adobe Photoshop</div>
-                  <div className={style.percent}>70%</div>
+                  <div className={style.percent}>60%</div>
               </div>
               <div className={style.wrapper}>
-                  <div className={style.percentage} style={{width:'70%', height: '18px', borderRadius:'6px'}}></div>
+                  <div className={style.percentage} style={{width:'60%', height: '18px', borderRadius:'6px'}}></div>
               </div>
           </div>
       </div>
