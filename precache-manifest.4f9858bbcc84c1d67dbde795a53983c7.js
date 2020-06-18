810e4b3c077b28239e2caa9159817e6e8abb9f8f
aidanakad <aydana0504@gmail.com>
2020-06-18 19:11:40 +0600

    Updates

---
+++ precache-manifest.4f9858bbcc84c1d67dbde795a53983c7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f8996e05618318cfb75535cfb555002",
+    "url": "/http//aidanakad.github.io/resume/index.html"
+  },
+  {
+    "revision": "32486d51488bb630f380",
+    "url": "/http//aidanakad.github.io/resume/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "160854ec63541e00b8ad",
+    "url": "/http//aidanakad.github.io/resume/static/css/main.868cc0d8.chunk.css"
+  },
+  {
+    "revision": "32486d51488bb630f380",
+    "url": "/http//aidanakad.github.io/resume/static/js/2.391de5e5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/http//aidanakad.github.io/resume/static/js/2.391de5e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "160854ec63541e00b8ad",
+    "url": "/http//aidanakad.github.io/resume/static/js/main.00d6b77f.chunk.js"
+  },
+  {
+    "revision": "89429dd8107c92d2ef89",
+    "url": "/http//aidanakad.github.io/resume/static/js/runtime-main.db920b07.js"
+  },
+  {
+    "revision": "0020c8baeb03e02dd010c8d3b2b7e675",
+    "url": "/http//aidanakad.github.io/resume/static/media/bg.0020c8ba.png"
+  },
+  {
+    "revision": "68e0cc37ff3e1b1de75e53f628d0bd6a",
+    "url": "/http//aidanakad.github.io/resume/static/media/personal-img-min.68e0cc37.JPG"
+  },
+  {
+    "revision": "0e65428a2bc48db2536d5881f87589db",
+    "url": "/http//aidanakad.github.io/resume/static/media/placeholder-portfolio.0e65428a.png"
+  },
+  {
+    "revision": "2a654408d2d94d0e0fbbcaa52d2e85d0",
+    "url": "/http//aidanakad.github.io/resume/static/media/todo.2a654408.png"
+  }
+]);
